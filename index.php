87cd21e423d588017a374c613d5c48ca4e021c56
Jhassir Candia <andruu11.jhass@gmail.com>
2019-03-22 01:26:09 -0400

    Clase 1.2 50%

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Train Theme</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <header class="header">
+            <div class="logo">
+                <a href="">Logo</a>
+            </div>
+            <nav class="menu">
+                <ul>
+                    <li><a href="">Seccion1</a></li>
+                    <li><a href="">Seccion2</a></li>
+                    <li><a href="">Seccion3</a></li>
+                    <li><a href="">Seccion4</a></li>
+                    <li><a href="">Seccion5</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="main">
+            <article class="content">
+                <h2>Contenido</h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi repellat ipsa dolores architecto quas consectetur, id nostrum rem nam sequi nulla temporibus necessitatibus, delectus omnis quae? Doloribus, sint exercitationem nam dignissimos fuga iure corporis sequi possimus labore eaque aliquid voluptatem, laborum dolorem error quasi libero magni! Nobis dolores amet at?
+               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur iusto ducimus quos nisi placeat soluta minus fuga ratione corrupti aspernatur rem, molestias autem error enim sint voluptatum voluptate quis? Quidem, est animi blanditiis voluptatum, obcaecati reprehenderit laudantium maxime nesciunt non recusandae labore possimus dignissimos? Culpa, aspernatur adipisci. At est dolore delectus autem harum rem, voluptates esse qui tempora, aspernatur totam deleniti quo illum suscipit fuga. Magni laudantium assumenda illo quae vitae repudiandae nobis et natus, voluptas quidem voluptatum ratione voluptatem quaerat sed unde provident iure sunt obcaecati quia cumque nostrum quo eum. Eos beatae ipsam tenetur animi, perferendis cupiditate eveniet assumenda enim incidunt nobis quos ut sapiente. Nesciunt repellat vero impedit voluptatem maiores. Doloribus laudantium, officia dolores cumque provident at sint illum odio fugiat recusandae facere et ullam ipsa error beatae ut ex, maxime optio consectetur. Quis eaque animi commodi nesciunt cum enim repellat doloremque rerum nostrum perspiciatis error voluptate repellendus, vitae unde eligendi facilis maxime deserunt aspernatur dignissimos, fugit sequi itaque. Necessitatibus fugit ad voluptatibus a corporis veniam eligendi quod officiis, provident officia quaerat eos tenetur iure dicta perspiciatis vero consequatur nam atque est repellendus obcaecati quam ea distinctio. Reiciendis, consequuntur dolorum magni amet fugiat, doloremque nesciunt facilis saepe commodi at atque minima. Incidunt iure officia doloribus ratione perferendis porro quia, id eligendi ipsum eos. Nobis ipsa odit reprehenderit pariatur, perferendis quae voluptate aperiam necessitatibus quisquam aut repellendus recusandae nesciunt facilis ex mollitia corporis deleniti ea culpa quam nam in autem vero voluptatem amet. Enim asperiores, ratione eum doloremque excepturi voluptate voluptatem, autem maiores atque labore, exercitationem esse. Reiciendis, dolore! Harum esse eaque blanditiis facere labore totam, nobis enim nam modi ipsa ipsum quidem velit expedita. Ipsa autem odio vel delectus molestias non, consequuntur architecto commodi veniam magnam est voluptatum. Sunt quam asperiores saepe eaque quis debitis velit doloremque! </p>
+            </article>
+            <aside>
+                <h2>Sidebar</h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae illum sit reiciendis repudiandae. Iste non illum debitis nemo facere corrupti numquam quae quisquam! Odio repellendus fugit accusamus harum doloremque quidem ratione corporis. Deleniti illum officia impedit quibusdam expedita facilis corporis eos enim incidunt, amet tempore, nam quisquam error similique facere? </p>
+            </aside>
+        </main>
+        <footer>
+            <nav class="social-media">
+                <ul>
+                    <li><a href="">Redes1</a></li>
+                    <li><a href="">Redes2</a></li>
+                    <li><a href="">Redes3</a></li>
+                    <li><a href="">Redes4</a></li>
+                    <li><a href="">Redes5</a></li>
+                </ul>
+            </nav>
+            <div>
+                <small>&copy; 2019 por @SummitCode</small>
+            </div>
+        </footer>
+    </body>
+</html>
